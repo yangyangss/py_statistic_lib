a32a10cbda627c31090141ac64d780dccef288ea
Yang Shulin <yang_shulin@tech.gov.sg>
2021-11-21 21:17:47 +0800

    add median, mode and mean functions

--- main_descriptive_stats.py
+++ main_descriptive_stats.py
@@ -1,11 +1,18 @@
 from collections import Counter
 
-from playStats.descriptive_stats import frequency
+from playStats.descriptive_stats import frequency, mode, median, mean
 
-if __name__ == '__main__':
 
-    data = [2, 2, 2, 2, 1, 1, 1, 3, 3]
+# 测试众数
+def test_mode(data):
+    mode_elements, mode_count = mode(data)
+    if mode_elements:
+        print('mode elements is : {}'.format(mode_elements))
+        print('mode count is : {}'.format(mode_count))
+    else:
+        print('Mode does not exist')
 
+def test_frequency(data):
     # 测试频数
     # Test Count
     counter = Counter(data)
@@ -13,10 +20,29 @@ if __name__ == '__main__':
 
     #get the count value of most common number
     print('most common [0][1] value is {}'.format(counter.most_common()[0][1]))
-
-
     # 测试频率
     # Test frequency
 
     freq = frequency(data)
     print('frequency value is : {}'.format(freq))
+
+def test_median(data):
+    print('median value is: {}'.format(median(data)))
+
+def test_mean(data):
+    print('mean value is: {}'.format(mean(data)))
+
+if __name__ == '__main__':
+    # data = [2, 2, 2, 2, 1, 1, 1, 3, 3]
+    # test_mode(data)
+
+    data = [1,4,2,3]
+    test_mean(data)
+
+
+    data = [1,4,2,3, 5]
+    test_mean(data)
+
+
+    data = [1,4,2,3, 5, 99]
+    test_mean(data)
--- playStats/descriptive_stats.py
+++ playStats/descriptive_stats.py
@@ -7,3 +7,47 @@ def frequency(data):
         ret.append((point[0], point[1] / len(data)))
 
     return ret
+
+
+def mode(data):
+    '''
+        find a
+        众数
+    '''
+    counter = Counter(data)
+    if counter.most_common()[0][1] == 1:
+        return None, None
+
+    count = counter.most_common()[0][1]
+    ret = []
+
+    for point in counter.most_common():
+        if point[1] == count:
+            ret.append(point[0])
+        else:
+            break
+
+    return ret, count
+
+def median(data):
+    '''
+    中位数
+    :param data:
+    :return: float value
+    '''
+    sorted_data = sorted(data)
+    n = len(sorted_data)
+
+    if n % 2 == 1:
+        return sorted_data[n // 2]
+
+    return (sorted_data[n // 2 - 1] + sorted_data[n // 2]) / 2
+
+def mean(data):
+    '''
+    均值
+    :param data:
+    :return:
+    '''
+
+    return sum(data) / len(data)
